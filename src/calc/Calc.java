f75c4a71c645f5feabcbf11d0d35ff006642ce91
Ravi <Ravi@DESKTOP-9P1MDVM>
2020-11-09 07:43:40 +0530

    Auto it

---
+++ src/calc/Calc.java
@@ -0,0 +1,16 @@
+package calc;
+
+import java.io.IOException;
+import org.testng.annotations.Test;
+
+public class Calc 
+{
+	@Test
+	
+	public void subtract() throws IOException
+	{
+		Runtime.getRuntime().exec("C:\\Users\\Ravi\\OneDrive\\Desktop\\cal1.exe");
+		//Run any executable file 
+	}
+
+}
